21ac294b3721ce10c342e6a8767ef488c457a69b
Icheka Ozuru <rhemafortune@gmail.com>
2022-11-15 22:01:47 +0100

    Allow both absolute and relative paths to renderer engine

--- src/preview.ts
+++ src/preview.ts
@@ -172,7 +172,10 @@ export default class Preview {
         // if the workspace is configured with a render engine, use that
         if (workspace.getConfiguration('mjml').rendererPath) {
             const workspaceDir = workspace.workspaceFolders![0].uri.path;
-            const renderer = path.join(workspaceDir, workspace.getConfiguration('mjml').rendererPath);
+            const rendererPath = workspace.getConfiguration('mjml').rendererPath;
+            
+            const renderer = path.isAbsolute(rendererPath) ? rendererPath : path.join(workspaceDir, workspace.getConfiguration('mjml').rendererPath);
+            
             const directory = path.dirname(document.uri.fsPath);
 
             const content = document.getText();
